bc62453473218d5db9bcb088a84e8202c32c1b6f
SystemicCypher <austinloza24@yahoo.com>
2017-05-09 20:43:14 -0700

    Added updated course catalog accessor
    Now only gets the course names

---
+++ Course catalog access/src/course/catalog/access/CourseCatalogAccess.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package course.catalog.access;
+
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.parser.*;
+import org.jsoup.examples.HtmlToPlainText;
+import org.jsoup.select.Elements;
+
+
+public class CourseCatalogAccess {
+    
+public static boolean isUpperCase(String s)
+{
+    for (int i=0; i<s.length(); i++)
+    {
+        if (!Character.isUpperCase(s.charAt(i)))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+    
+    public static void main(String[] args) throws Exception {
+           Document newDoc = Jsoup.connect("https://my.sa.ucsb.edu/Public/curriculum/coursesearch.aspx").get();
+           
+           HtmlToPlainText test = new HtmlToPlainText();
+           Scanner parsing = new Scanner(test.getPlainText(newDoc));
+           while(parsing.hasNext()){
+               String read = parsing.next();
+               if(read.equals("FALL")) break;
+               if(isUpperCase(read)){
+                   System.out.print(read + " "); 
+                   String nexRead = parsing.next();
+                   if(isUpperCase(nexRead))
+                       System.out.println(nexRead);
+                   else
+                       System.out.print("\n");
+                   
+                
+               }
+           }
+           
+    }
+}
+    
+
--- HTML Access Library/apidocs/javadoc.bat
+++
@@ -1,1 +0,0 @@
-C:\Progra~1\Java\jdk1.8.0_121\jre\..\bin\javadoc.exe @options @packages
